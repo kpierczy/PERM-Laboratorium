e0f04ba7e421cdf2a42b0debe34d0aab4c086f1b
Marcin <marmichalski97@gmail.com>
2020-05-14 02:25:03 +0200

    popsulem gita

--- Lab_3/savePlot.m
+++
@@ -1,9 +0,0 @@
-function [] = savePlot(name)
-%prints current plot to name.pdf
-h = gcf;
-set(h,'Units','Inches');
-pos = get(h,'Position');
-set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-print(h,name,'-dpng','-r1000')
-end
-
---
+++ Lab_4/scripts/camera/cameras_calibration.m
@@ -0,0 +1,7 @@
+plot = true;
+
+camera_calibration_wide;
+% camera_calibration_medium;
+% camera_calibration_narrow;
+
+clearvars -except cameraParamsNarrow cameraParamsWide cameraParamsMedium
